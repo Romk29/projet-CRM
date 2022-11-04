92b43bcd96c1f4f95c32a41d024d02f1a7816ad5
Romain Kerhervé <romain.kerherve@gmail.com>
2022-11-04 13:34:40 +0100

    correction

--- EXO base w3school.sql
+++ EXO base w3school.sql
@@ -1,34 +1,34 @@
 //EXO base w3school
 
 // 1 Récupérer le nombre de customers par ville.
-SELECT City, COUNT(CustomerID)
-FROM [Customers]
+SELECT City, COUNT(CustomerID) AS nb_client_ville
+FROM Customers
 GROUP BY City
 ORDER BY COUNT(CUSTOMERID) DESC
 
 //2 Récupérer le nom de chaque produit présent dans une Order
-SELECT Orders.OrderID, Products.ProductName 
-FROM Products,Orders,OrderDetails
-WHERE OrderDetails.OrderID = Orders.OrderID
-AND OrderDetails.ProductID = Products.ProductID
+SELECT OrderDetails.OrderID, Products.ProductName 
+FROM OrderDetails, Products
+WHERE OrderDetails.ProductID = Products.ProductID
 
 //3 Récupérer tous les produits de la catégorie "Beverages"
-SELECT CategoryName, Products.ProductName 
+SELECT Products.ProductName 
 FROM Categories,Products
 WHERE Categories.CategoryID = Products.CategoryID
 AND Categories.CategoryName = 'Beverages'
 
 //4 Compter le nombre de clients ayant une tarte au sucre dans leurs commandes
 SELECT COUNT(Customers.CustomerName) AS Client_avec_tarte_au_sucre
-FROM Customers, Orders, OrderDetails 
+FROM Customers, Orders, OrderDetails, Products 
 WHERE Customers.CustomerID = Orders.CustomerID
 AND Orders.OrderID = OrderDetails.OrderID
-AND OrderDetails.ProductID = 62
+AND OrderDetails.ProductID = Products.ProductID
+AND Products.ProductName = "Tarte au sucre"
 
 
-//5 Compter le nombre de Sirop d'érable présent dans des commandes
+//5 Compter le nombre de Sirop dérable présent dans des commandes
 SELECT SUM(OrderDetails.Quantity) AS Nbr_de_sirop_d_érable_dans_les_commandes
 FROM OrderDetails, Products
 WHERE OrderDetails.ProductID = Products.ProductID
-AND Products.ProductID = 61
+AND Products.ProductName = "Sirop d'érable"
 
