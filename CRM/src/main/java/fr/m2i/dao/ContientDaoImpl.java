30813fb361b4fff215eb6756cb9e07e6590d6e15
Morgane Salaün <salaun.mrgn@gmail.com>
2022-11-10 16:39:48 +0100

    ContientDAO

--- CRM/src/main/java/fr/m2i/dao/ContientDaoImpl.java
+++ CRM/src/main/java/fr/m2i/dao/ContientDaoImpl.java
@@ -1,5 +1,39 @@
 package fr.m2i.dao;
 
+import java.util.List;
+
+import fr.m2i.model.Contient;
+
 public class ContientDaoImpl implements ContientDao {
+
+	@Override
+	public void creer(Contient contient) throws DaoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Contient trouver(int id) throws DaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Contient> lister() throws DaoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void supprimer(int id) throws DaoException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void miseAJour(Contient contient) throws DaoException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
