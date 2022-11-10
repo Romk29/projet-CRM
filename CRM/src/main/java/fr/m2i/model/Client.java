6f9cc3d7d3f91a620f9c425e3bdd59ca71cb128b
Laura Dufaut <laura_dufaut@yahoo.fr>
2022-11-10 15:45:45 +0100

    ajout classe client

--- CRM/src/main/java/fr/m2i/model/Client.java
+++ CRM/src/main/java/fr/m2i/model/Client.java
@@ -1,5 +1,95 @@
 package fr.m2i.model;
 
 public class Client {
+	
+	private int id;
+	private String nom;
+	private String prenom;
+	private Adresse adresse;
+	private String nomSociete;
+	private String telephone;
+	private int etat;
+	private int genre;
+	
+	public Client() {
+		
+	}
+
+	public Client(String nom, String prenom, Adresse adresse, String nomSociete, String telephone, int etat,
+			int genre) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.nomSociete = nomSociete;
+		this.telephone = telephone;
+		this.etat = etat;
+		this.genre = genre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getNomSociete() {
+		return nomSociete;
+	}
+
+	public void setNomSociete(String nomSociete) {
+		this.nomSociete = nomSociete;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public int getEtat() {
+		return etat;
+	}
+
+	public void setEtat(int etat) {
+		this.etat = etat;
+	}
+
+	public int getGenre() {
+		return genre;
+	}
+
+	public void setGenre(int genre) {
+		this.genre = genre;
+	}
+	
 
 }
