0b1118857a23b608f363621f078905e9a898dd44
Joatan Le-Lostec <lelostecj@hotmail.fr>
2022-11-17 15:37:46 +0100

    ajouter paiement

--- CRM/src/main/java/fr/m2i/servlet/AjouterPaiement.java
+++ CRM/src/main/java/fr/m2i/servlet/AjouterPaiement.java
@@ -1,6 +1,10 @@
 package fr.m2i.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -44,25 +48,56 @@ public class AjouterPaiement extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
+		
+		Map<String, String> erreurs = new HashMap<String, String>();
 		int idClient = Integer.parseInt(request.getParameter("clientPaiement"));
+		
+		
+		
 		String noCarte = request.getParameter("noCartePaiement");
 		String codeConfidentiel = request.getParameter("codeConfidentielPaiement");
 		String banque = request.getParameter("banquePaiement");
 
-			Paiement paiement = new Paiement();
-			paiement.setClient(clientDao.trouver(idClient));
-			paiement.setNoCarte(noCarte);
-			paiement.setCodeConfidentiel(codeConfidentiel);
-			paiement.setBanque(banque);
-			paiementDao.creer(paiement);
-			
-		} catch (DaoException e) {
-			e.printStackTrace();
+		if (banque == null) {
+
+			erreurs.put("banquePaiment", "Merci d'entrer une banque.");
+		}
+		Pattern patternAdress1 = Pattern.compile("[0-9]{3}");
+
+		if (!patternAdress1.matcher(codeConfidentiel).matches()) {
+
+			erreurs.put("codeConfidentielPaiement", "votre code confidentiel doit faire 3 choffre");
+		}
+
+		Pattern patternAdress = Pattern.compile("[0-9]{16}");
+		if (!patternAdress.matcher(noCarte).matches()) {
+
+			erreurs.put("noCartePaiement", "merci de rentrer une carte a 16 chiffre");
 		}
 		
-		response.sendRedirect( request.getContextPath() + "/ListePaiements" );		
-	}
+				
+		Paiement paiement = new Paiement();
+		if (erreurs.isEmpty()) {		
+			try {
+				paiement.setClient(clientDao.trouver(idClient));
+				paiement.setNoCarte(noCarte);
+				paiement.setCodeConfidentiel(codeConfidentiel);
+				paiement.setBanque(banque);
+				paiementDao.creer(paiement);
+			} catch (DaoException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			request.setAttribute("adresse", paiement);
+			request.setAttribute("erreurs", erreurs);
+
+			this.getServletContext().getRequestDispatcher("/WEB-INF/ajouterPaiement.jsp").forward(request, response);
+		}
+			
+		
+		
+		}
 		
 			
 		
