e06bef890a0bfc38619b3c476edca24856549919
Morgane Salaün <salaun.mrgn@gmail.com>
2022-11-17 12:28:48 +0100

    erreurs Ajouter Produit panier

--- CRM/src/main/java/fr/m2i/servlet/AjouterProduit.java
+++ CRM/src/main/java/fr/m2i/servlet/AjouterProduit.java
@@ -1,6 +1,9 @@
 package fr.m2i.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -43,25 +46,53 @@ public class AjouterProduit extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
+		Map<String, String> erreurs = new HashMap<String, String>();
 		
 		String nom = request.getParameter("nom");
 		double prix = Double.parseDouble(request.getParameter("prix"));
 		String description = request.getParameter("description");
+		
+		if(nom != null) {
+			if(nom.length() < 2 || nom.length() > 50) {
+				erreurs.put("nomProduit", "Un nom doit contenir entre 2 et 50 caractères.");
+			}
+		} else {
+			erreurs.put("nomProduit", "Merci d'entrer un nom.");
+		}
+		
+		if(description != null) {
+			if(description.length() > 2 || description.length() > 200 ) {
+				erreurs.put("descriptionProduit", "Une description doit contenir entre 2 et 200 caractères.");
+			}
+			
+		if(prix == 0) {
+			erreurs.put("prixProduit", "Merci d'entrer un prix");
+		}
+		
+
 
 			Produit produit = new Produit();
 			produit.setNom(nom);
 			produit.setPrix(prix);
 			produit.setDescription(description);
-			
-			produitDao.creer(produit);
-			
-		} catch (DaoException e) {
-			e.printStackTrace();
-		}
-		
-		response.sendRedirect( request.getContextPath() + "/ListeProduits" );	
 
+			if(erreurs.isEmpty()) {
+				try {
+					produitDao.creer(produit);
+					
+					request.getSession().setAttribute("confirmMessage", "Le produit a bien été ajouté !");
+				} catch (DaoException e) {
+					e.printStackTrace();
+				}
+				
+				response.sendRedirect( request.getContextPath() + "/ListeProduits" );
+			} 
+				request.setAttribute("produit", produit);
+				request.setAttribute("erreurs", erreurs);
+				
+				this.getServletContext().getRequestDispatcher("/WEB-INF/ajouterProduit.jsp").forward(request, response);
+		}
 	}
 
 }
+
