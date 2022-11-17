28c2aefcae6b08f63740c106aa0a6d7a5cfe4c4e
Joatan Le-Lostec <lelostecj@hotmail.fr>
2022-11-17 10:22:14 +0100

    supprimer Produit

--- CRM/src/main/java/fr/m2i/servlet/SupprimerProduit.java
+++ CRM/src/main/java/fr/m2i/servlet/SupprimerProduit.java
@@ -7,18 +7,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.m2i.dao.AdresseDao;
+import fr.m2i.dao.DaoException;
+import fr.m2i.dao.DaoFactory;
+
 /**
  * Servlet implementation class SupprimerProduit
  */
 @WebServlet("/SupprimerProduit")
 public class SupprimerProduit extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private AdresseDao adresseDao;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public SupprimerProduit() {
-        super();
+    	super();
+    	adresseDao = DaoFactory.getInstance().getAdresseDao();
+    	
         // TODO Auto-generated constructor stub
     }
 
@@ -26,8 +34,17 @@ public class SupprimerProduit extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			int id = Integer.parseInt(request.getParameter("id"));
+			adresseDao.supprimer(id);
+			
+			//Ajout d'un élément dans la session
+			request.getSession().setAttribute("confirmMessage", "L'adresse a bien été supprimé !");
+		} catch (DaoException e) {
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect( request.getContextPath() + "/ListeAdresses" );
 	}
 
 	/**
