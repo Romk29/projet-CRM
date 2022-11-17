fbc7f9ca2cf99d675210623ffea6d0a90df3a6c0
Romain Kerhervé <romain.kerherve@gmail.com>
2022-11-17 13:07:30 +0100

    Add gestion erreurs Client

--- CRM/src/main/java/fr/m2i/servlet/AjouterClient.java
+++ CRM/src/main/java/fr/m2i/servlet/AjouterClient.java
@@ -1,6 +1,9 @@
 package fr.m2i.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -53,34 +56,100 @@ public class AjouterClient extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		Map<String, String> erreurs = new HashMap<String, String>();
+		
 		String nom = request.getParameter("nomClient");
 		String prenom = request.getParameter("prenomClient");
-		int idAdresse = Integer.parseInt(request.getParameter("adresseClient"));
+		
+		Adresse adresse = null;
+		try {
+			int idAdresse = Integer.parseInt(request.getParameter("adresseClient"));
+			adresse = adresseDao.trouver(idAdresse);
+		} catch (DaoException | NumberFormatException e) {
+			erreurs.put("adresseClient", "Erreur l'adresse n'existe pas.");
+		}
+				
+		
 		String nomSociete = request.getParameter("nomSocieteClient");
 		String mail = request.getParameter("mailClient");
 		String telephone = request.getParameter("telephoneClient");
-		int etat = Integer.parseInt(request.getParameter("etatClient"));
-		int genre = Integer.parseInt(request.getParameter("genreClient"));
+		
+		int etat = 0;
+		int genre = 0;
+		
+		if (nom.length() == 0) {
+			erreurs.put("nomClient", "Merci d'entrer un nom.");
+		}
+		
+		if (prenom.length() == 0) {
+			erreurs.put("prenomClient", "Merci d'entrer un prenom.");
+		}
+		
+		if(mail != null) {
+			if(mail.length() > 60) {
+				erreurs.put("emailClient", "Un email doit avoir maximum 60 caractères.");
+			}
+			if(!mail.matches("([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)")) {
+				erreurs.put("emailClient", "Merci d'entrer une adresse email valide.");
+			}
+		}
+		
+		if(telephone != null) {
+			if(telephone.length() > 10) {
+				erreurs.put("telephoneClient", "Un numéro de téléphone doit avoir maximum 10 caractères.");
+			}
+			if(!telephone.matches("^\\d+$")) {
+				erreurs.put("telephoneClient", "Un numéro de téléphone doit contenir uniquement des chiffres.");
+			}
+		} else {
+			erreurs.put("telephoneClient", "Merci d'entrer un numéro de téléphone.");
+		}
+		
+		try {
+			etat = Integer.parseInt(request.getParameter("etat"));
+		} catch (NumberFormatException e) {
+			erreurs.put("etatClient", "Merci d'entrer 1 pour un 'propre' ou 2 pour 'sale' .");
+		}
+		
+		try {
+			genre = Integer.parseInt(request.getParameter("genre"));
+		} catch (NumberFormatException e) {
+			erreurs.put("genreClient", "Merci d'entrer 1 pour 'homme', 2 pour 'femme' ou 3 pour 'autre'.");
+		}
+		
+		
 		
 		Client client = new Client();
+		client.setNom(nom);
+		client.setPrenom(prenom);
+		client.setAdresse(adresse);
+		client.setNomSociete(nomSociete);
+		client.setMail(mail);
+		client.setTelephone(telephone);
+		client.setEtat(etat);
+		client.setGenre(genre);
+		
+		if (erreurs.isEmpty()) {
+				
 		try {
-			client.setNom(nom);
-			client.setPrenom(prenom);
-			Adresse adresse = adresseDao.trouver(idAdresse);
-			client.setAdresse(adresse);
-			client.setNomSociete(nomSociete);
-			client.setMail(mail);
-			client.setTelephone(telephone);
-			client.setEtat(etat);
-			client.setGenre(genre);
 			
 			clientDao.creer(client);
+			request.getSession().setAttribute("confirmMessage", "Le client a bien été ajouté !");
+			
+			response.sendRedirect(request.getContextPath() + "/ListeClients");
 		} catch(DaoException e) {
 			e.printStackTrace();
 		}
 		
-		response.sendRedirect(request.getContextPath() + "/ListeClients");
+		} else {
+			request.setAttribute("client", client);
+			request.setAttribute("erreurs", erreurs);
+
+			this.getServletContext().getRequestDispatcher("/WEB-INF/ajouterClient.jsp").forward(request, response);
+		}
+		
+	}
 		
 	}
 				
-}
+
