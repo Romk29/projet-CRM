0d3ddcffa9c213cac986a85d5953da599217b65e
Romain Kerhervé <romain.kerherve@gmail.com>
2022-11-25 11:39:36 +0100

    add dao impl

---
+++ CRMV2/src/main/java/fr/m2i/dao/DaoObject.java
@@ -0,0 +1,122 @@
+package fr.m2i.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityTransaction;
+import javax.persistence.NoResultException;
+
+public abstract class DaoObject<T> {
+
+	private DaoFactory factory;
+	private Class<T> className;
+
+	public DaoObject(Class<T> className) {
+		this.factory = DaoFactory.getInstance();
+		this.className = className;
+	}
+
+
+	public T find(long id) throws DaoException {
+		T object = null;
+
+		try {
+			object = factory.getEntityManager().find(className, id);
+
+		} catch(NoResultException e) {
+			e.printStackTrace();
+			throw new DaoException("Erreur DAO");
+		} finally {
+			factory.releaseEntityManager();
+		}
+
+		if(object == null ) {
+			throw new DaoException("Elément non trouvé");
+		}
+
+		return object;
+	}
+
+	public List<T> list() {
+		List<T> objects = null;
+
+		try {
+			objects = factory.getEntityManager().createQuery("FROM "+className.getCanonicalName(), className).getResultList();
+		} finally {
+			factory.releaseEntityManager();
+		}
+
+		return objects;
+	}
+
+	public void create(T object) throws DaoException {
+		EntityTransaction trans = null;
+
+		try {
+			trans = factory.getEntityManager().getTransaction();
+			trans.begin();
+
+			factory.getEntityManager().persist(object);
+
+			trans.commit();
+		} catch(Exception e) {
+			if(trans != null)
+				trans.rollback();
+
+			e.printStackTrace();
+			throw new DaoException("Erreur DAO");
+		} finally {
+			factory.releaseEntityManager();
+		}
+	}
+
+	public void update(T object) throws DaoException {
+		EntityTransaction trans = null;
+
+		try {
+			trans = factory.getEntityManager().getTransaction();
+			trans.begin();
+
+			factory.getEntityManager().merge(object);
+
+			trans.commit();
+		} catch(Exception e) {
+			if(trans != null)
+				trans.rollback();
+
+			e.printStackTrace();
+			throw new DaoException("Erreur DAO");
+		} finally {
+			factory.releaseEntityManager();
+		}
+	}
+
+	public void delete(long id) throws DaoException {
+		EntityTransaction trans = null;
+
+		try {
+			trans = factory.getEntityManager().getTransaction();
+			trans.begin();
+
+			T object = factory.getEntityManager().find(className, id);
+
+			factory.getEntityManager().remove(object);
+
+			trans.commit();
+		} catch(NoResultException e) {
+			throw new DaoException("Erreur DAO");
+		} catch(Exception e) {
+			if(trans != null)
+				trans.rollback();
+
+			e.printStackTrace();
+			throw new DaoException("Erreur DAO");
+		} finally {
+			factory.releaseEntityManager();
+		}
+	}
+
+
+	public DaoFactory getFactory() {
+		return factory;
+	}
+}
---
+++ CRMV2/src/main/java/fr/m2i/dao/impl/DaoAdresse.java
@@ -0,0 +1,12 @@
+package fr.m2i.dao.impl;
+
+import fr.m2i.dao.DaoObject;
+import fr.m2i.model.Adresse;
+
+public class DaoAdresse extends DaoObject<Adresse> {
+
+	public DaoAdresse() {
+		super(Adresse.class);
+	}
+
+}
